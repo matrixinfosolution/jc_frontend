2557b7ca4496cb60362b22fe5efd3bf8fc519008
NemishteAnushka <nemishte.anushka23@gmail.com>
2024-11-09 21:33:07 +0530

    completed footer and imgslider

---
+++ src/Components/pages/EnquiryForm.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Col, Form, FormControl, Row } from "react-bootstrap";
+
+function EnquiryForm() {
+  return (
+    <Form>
+      <Row>
+        <Col>
+          <FormControl
+            type="text"
+            placeholder="Name"
+            className="bg-transparent "
+          />
+        </Col>
+        <Col>
+          <FormControl
+            type="text"
+            placeholder="Mobile Number"
+            className="bg-transparent "
+          />
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col>
+          <FormControl
+            as="textarea"
+            rows={1}
+            placeholder="Description"
+            className="bg-transparent"
+          />
+        </Col>
+      </Row>
+      <div className="d-flex justify-content-end mt-2 mb-1">
+        <Button className="bg-success border-0">Submit</Button>
+      </div>
+    </Form>
+  );
+}
+
+export default EnquiryForm;
--- src/Components/pages/Footer.jsx
+++ src/Components/pages/Footer.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import EnquiryForm from "./EnquiryForm";
 
 function Footer() {
   return (
-    <div>
-      <h2>Footer</h2>
+    <div className="footer-bg">
+      <Row>
+        <Col style={{ color: "white" }}>
+          <h2 style={{ textAlign: "center" }}>Screen</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto ad,
+            tempora provident sint odio atque ullam voluptatibus, voluptate et
+            dolorum consequuntur.
+          </p>
+        </Col>
+        <Col style={{ color: "white" }}>
+          <h2 style={{ textAlign: "center" }}>Shop</h2>
+          <ul>
+            <li>Screen Printing</li>
+            <li>DTG</li>
+            <li>Heat press printing</li>
+            <li>DYE Sublimation</li>
+          </ul>
+        </Col>
+        <Col style={{ color: "white" }}>
+          <h2 style={{ textAlign: "center" }}>Links</h2>
+          <ul>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Terms & Conditions</li>
+            <li>Privacy & Policy</li>
+          </ul>
+        </Col>
+        <Col style={{ color: "white" }}>
+          <h2 style={{ textAlign: "center" }}>Enquiry</h2>
+          <EnquiryForm />
+        </Col>
+      </Row>
+      <hr className="custom-hr" />
+      <div className="d-flex justify-content-center align-items-center text-white">
+        <p>2024 , All Rights Reserved.</p>
+      </div>
     </div>
   );
 }
